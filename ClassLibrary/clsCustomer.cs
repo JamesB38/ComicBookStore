62ab3f478aba91be291762304da9801412f41205
Diogo Rodrigues <P2532455@my365.dmu.ac.uk>
2021-02-13 15:47:06 +0000

    Created properties for my table design

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool EmailSubscription { get; set; }
+        public string CustomerFullName { get; set; }
+        public long CustomerPhoneNumber { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime CustomerDOB { get; set; }
     }
 }
--- Testing1/tstCustomer.cs
+++ Testing1/tstCustomer.cs
@@ -14,5 +14,69 @@ namespace Testing1
 
             Assert.IsNotNull(ACustomer);
         }
+
+        [TestMethod]
+        public void EmailSubPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            Boolean TestData = true;
+
+            ACustomer.EmailSubscription = TestData;
+
+            Assert.AreEqual(ACustomer.EmailSubscription, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerFNPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            string TestData = "Diogo Rodrigues";
+
+            ACustomer.CustomerFullName = TestData;
+
+            Assert.AreEqual(ACustomer.CustomerFullName, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerPNoPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            long TestData = 07415564047;
+
+            ACustomer.CustomerPhoneNumber = TestData;
+
+            Assert.AreEqual(ACustomer.CustomerPhoneNumber, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            int TestData = 0004;
+
+            ACustomer.CustomerID = TestData;
+
+            Assert.AreEqual(ACustomer.CustomerID, TestData);
+        }
+
+
+        [TestMethod]
+        public void CustomerDOBPropertyOK()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            ACustomer.CustomerDOB = TestData;
+
+            Assert.AreEqual(ACustomer.CustomerDOB, TestData);
+        }
+
+
+
     }
 }
