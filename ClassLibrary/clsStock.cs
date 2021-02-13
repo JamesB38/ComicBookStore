0da813972382eb0d84e3a565fcd85909765ce172
James Brass <P2533170@my365.dmu.ac.uk>
2021-02-13 18:12:53 +0000

    Added testing and class for stock management

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary
+{
+    public class clsStock
+    {
+        public clsStock()
+        {
+        }
+    }
+}
---
+++ Testing2/tstStock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStock AnStock = new clsStock();
+
+            Assert.IsNotNull(AnStock);
+
+
+        }
+    }
+}
