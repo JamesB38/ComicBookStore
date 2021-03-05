7d68426f5bc4ef8fcca8e5ea5bc17fabf49bc8e9
James Brass <P2533170@my365.dmu.ac.uk>
2021-03-05 15:15:45 +0000

    Added find method for presentation layer

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -25,4 +25,25 @@ public partial class _1_DataEntry : System.Web.UI.Page
     
         Response.Redirect("StockViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsStock AnStock = new clsStock();
+
+        Int32 StockID;
+
+        Boolean Found = false;
+
+        StockID = Convert.ToInt32(txtStockID.Text);
+
+        Found = AnStock.Find(StockID);
+
+        if(Found == true)
+        {
+            txtStockAvailability.Text = AnStock.StockAvailability.ToString();
+            txtStockDescription.Text = AnStock.StockDescription;
+            txtStockPrice.Text = AnStock.StockPrice.ToString();
+     
+        }
+    }
 }
