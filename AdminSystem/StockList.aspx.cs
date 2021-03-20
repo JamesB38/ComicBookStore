15c72b27e9451b0c939d19a71faf28d2d08f0ddb
James Brass <P2533170@my365.dmu.ac.uk>
2021-03-20 22:23:07 +0000

    Added stockCollection class data and presentation layer

--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        if(IsPostBack == false)
+        {
+            DisplayDescriptions();
+        }
+
+    }
+
+    void DisplayDescriptions()
+    {
+        clsStockCollection Stocks = new clsStockCollection();
+
+        lstStockList.DataSource = Stocks.StockList;
+
+        lstStockList.DataValueField = "StockID"; //primary key
+
+        lstStockList.DataTextField = "StockDescription"; //data thats going to be shown
+
+        lstStockList.DataBind();
     }
 }
---
+++ Testing2/tstStockCollection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStockCollection {
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+
+            Assert.IsNotNull(AllStock);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+
+            List<clsStock> testList = new List<clsStock>();
+            clsStock testItem = new clsStock();
+
+            testItem.StockID = 1;
+            testItem.StockAvailability = 10;
+            testItem.StockDescription = "Comic Book";
+            testItem.StockPrice = 9.99;
+            testItem.IsBeingRestocked = true;
+
+            testList.Add(testItem);
+
+            allStock.StockList = testList;
+
+            Assert.AreEqual(allStock.StockList, testList);
+        }
+        
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+
+            
+            clsStock testItem = new clsStock();
+
+            testItem.StockID = 1;
+            testItem.StockAvailability = 10;
+            testItem.StockDescription = "Comic Book";
+            testItem.StockPrice = 9.99;
+            testItem.IsBeingRestocked = true;
+
+            allStock.ThisStock = testItem;
+
+            Assert.AreEqual(allStock.ThisStock, testItem);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection allStock = new clsStockCollection();
+
+            List<clsStock> testList = new List<clsStock>();
+            clsStock testItem = new clsStock();
+
+            testItem.StockID = 1;
+            testItem.StockAvailability = 10;
+            testItem.StockDescription = "Comic Book";
+            testItem.StockPrice = 9.99;
+            testItem.IsBeingRestocked = true;
+
+            testList.Add(testItem);
+
+            allStock.StockList = testList;
+
+            Assert.AreEqual(allStock.Count, testList.Count);
+        }
+       
+
+    }
+
+    
+}
